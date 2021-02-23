3285f32d602536bc10654876881b1e35143fadda
brolinr <brolinremz@gmail.com>
2021-02-23 04:48:06 +0200

    Users and Posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @friends = current_user.friends
+    @pending_requests =current_user.pending_requests
+    @friend_request - current_user.received_requests
   end
 
   def show
+    @user = user.find(params[:id])
   end
+
+  def update_img
+    @user = User.find(params[:id])
+    if image
+      @user.image =imge
+      if @user.save
+        flash[:success] = "Image uploaded"
+      else
+        flash[:danger] = "Image upload failed"
+      end
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,57 @@
 class User < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and   
-    # :omniauthable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
-            :omniauthable, omniauth_providers: %i[facebook]
-    validate :picture_size
-    
-    private
-    # Validates the size of an uploaded picture.
-    def picture_size
-      errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
+
+  mount_uploader :image, ImageUploader
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and 
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+  has_many :posts
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :friend_sent, class_name: 'Friendship',
+           foreign_key: 'sent_by_id',
+           inverse_of: 'sent_by',
+           dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship',
+           foreign_key: 'sent_to_id',
+           inverse_of: 'sent_to',
+           dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) },
+           through: :friend_sent, source: :sent_to
+  has_many :pending_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_sent, source: :sent_to
+  has_many :received_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_request, source: :sent_by
+  has_many :notifications, dependent: :destroy
+ # mount_uploader :image, PictureUploader
+  validate :picture_size
+
+  # Returns a string containing this user's first name and last name
+  def full_name
+    "#{fname} #{lname}"
+  end
+
+  # Returns all posts from this user's friends and self
+  def friends_and_own_posts
+    myfriends = friends
+    our_posts = []
+    myfriends.each do |f|
+      f.posts.each do |p|
+        our_posts << p
+      end
     end
+    posts.each do |p|
+      our_posts << p
+    end
+    our_posts
+  end
+
+  private
+  
+  # Validates the size of an uploaded picture.
+  def picture_size
+    errors.add(:image, 'should be less than 1MB') if image.size > 
+    1.megabytes
+  end
 end
